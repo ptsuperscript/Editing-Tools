f0a9a272f881c45c867abd10a430413c27d9781f
ptsuperscript <61100846+ptsuperscript@users.noreply.github.com>
2020-02-16 12:01:07 -0700

    Update Package.py
    Working on developing the project

--- Package.py
+++ Package.py
@@ -1,2 +1,56 @@
 # Editing-Tools
-Software for MS Cleanup
+#Software for MS Cleanup
+
+#front matter stats
+print("how many pages in the front matter?")
+fm_page_count = input()
+print("How many images in the front matter?")
+fm_image_count = input()
+print("How many notes in the front matter?")
+fm_note_count = input()
+print("How many tables in the front matter?")
+fm_table_count = input()
+print("How many characters in the front matter?")
+fm_table_count = input()
+
+
+
+#chapter 1 stats
+print("Is there a chapter 1?")
+chapter_1 = input()
+if chapter_1 == "yes" or "Yes":
+  print("How many pages in Chapter 1?")
+  chapter_1_page_count = input()
+  print("How many images in Chapter 1?")
+  chapter_1_image_count = input()
+  print("How many notes in Chapter 1?")
+  chapter_1_note_count = input()
+  print("How many tables in Chapter1?")
+  chapter_1_table_count = input()
+  print("How many characters in chapter 1?")
+  chapter_1_character count = input()
+  
+print("Is there a chapter 2?")
+chapter_2 = input()
+if chapter_2 = "yes" or "Yes":
+  print("How many pages in Chapter 2?")
+  chapter_2_page_count = input()
+  print("How many images in Chapter 2?")
+  chapter_2_image_count = input()
+  print("How many notes in Chapter 2?")
+  chapter_2_note_count = input()
+  print("How many tables in Chapter2?")
+  chapter_2_table_count = input()
+  print("How many characters in chapter 2?")
+  chapter_2_character count = input()
+elif chapter_2 = "no" or "No":
+  Ms_stats (fm, ch_1,)
+  
+
+
+  
+
+
+
+
+
